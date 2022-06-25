5dcc589338f5e1f2d74e4b457794b7ec3dbb834c
jayasai183 <jayasaivardhan.jv@gmail.com>
2022-06-25 11:59:23 +0530

    modified

--- server/index.js
+++ server/index.js
@@ -7,12 +7,6 @@ const list = {};
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
-
 app.post("/", (req, res) => {
     const id = shortid.generate();
     list[id] = req.body.longUrl;
